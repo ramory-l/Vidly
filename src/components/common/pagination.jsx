cc65834e43969b696627ce29c1e18b76b138388f
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-10-12 00:27:02 +0300

    no warnings

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
+import { Link } from "react-router-dom";
 
 const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   const pagesCount = Math.ceil(itemsCount / pageSize);
@@ -14,9 +15,9 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
             key={page}
             className={page === currentPage ? "page-item active" : "page-item"}
           >
-            <a className="page-link" onClick={() => onPageChange(page)}>
+            <Link className="page-link" onClick={() => onPageChange(page)}>
               {page}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
