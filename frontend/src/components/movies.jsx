39acba9b51075e158cdb21aedf0751754c0145e4
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-10-14 20:40:27 +0300

    added auth

--- frontend/src/components/movies.jsx
+++ frontend/src/components/movies.jsx
@@ -96,6 +96,8 @@ class Movies extends Component {
   render() {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { user } = this.props;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
@@ -110,11 +112,13 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link to="/movies/new">
-            <button type="button" className="btn btn-primary mb-3">
-              New Movie
-            </button>
-          </Link>
+          {user && (
+            <Link to="/movies/new">
+              <button type="button" className="btn btn-primary mb-3">
+                New Movie
+              </button>
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
